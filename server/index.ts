0ec5db4bbf1d3041a2c5af04aa69d3f9c551a48f
adi356 <75458420+adi356@users.noreply.github.com>
2024-05-08 20:08:06 -0600

    moved .env into server got getAllPostsController implemented

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,23 @@ import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+import { getAllPostsController } from './controllers'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
 
+console.log(process.env.PORT)
+
 app.use(bodyParser())
 
+app.get('/all-posts', getAllPostsController)
+
+mongoose.connect(process.env.CONNECTION_STRING!).then(() => {
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+    })
+}).catch(e => {
+    console.log('Connection failed')
+})
